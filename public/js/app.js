ecaf67527ffdad36586e624cfba0513cc823f6ab
Sarsenovz2z <nurik9293709@gmail.com>
2019-02-19 17:10:03 +0600

    auth page mob done

--- public/js/app.js
+++ public/js/app.js
@@ -49145,26 +49145,14 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/sass/chat.scss":
-/*!**********************************!*\
-  !*** ./resources/sass/chat.scss ***!
-  \**********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ 0:
-/*!****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/chat.scss ./resources/sass/app.scss ***!
-  \****************************************************************************************/
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /var/www/work/aiguardians/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /var/www/work/aiguardians/resources/sass/chat.scss */"./resources/sass/chat.scss");
 module.exports = __webpack_require__(/*! /var/www/work/aiguardians/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
